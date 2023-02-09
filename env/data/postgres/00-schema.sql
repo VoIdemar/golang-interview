04af76e816559d336069aea343aecef729373818
Vladimir Pankov <pankovfam@mail.ru>
2023-02-09 09:52:57 +0300

    Add source code

--- env/data/postgres/00-schema.sql
+++ env/data/postgres/00-schema.sql
@@ -1,31 +1,3 @@
-# Introduction
-
-This code has problems and does not meet our SLA.
-
-Let's find all the issues and make it great.
-
-# Overview
-
-Program is divided on 2 parts: data generator and loader. Data generator will generate provided amount of randomly
-generated events with following probability:
-
-* type 1 - 15%
-* type 2 - 20%
-* type 3 - 20&
-* type 5 - 45%
-
-All the data must be randomly generated, there are no preferences of generation algorithm.
-
-Events must be stored on local file system for future loading.
-
-Loading will be done by separate process - loader. That process should insert this data to database.
-
-Our goal is to generate 8M of events and load them in database as soon as it is possible. Hardware - developer machine.
-
-# Table Structure
-
-```sql
-
 create table event
 (
     event_source     text not null, -- unique identifier of client
@@ -66,4 +38,3 @@ create index event_event_type_index
 
 create index event_location_index
     on event (location);
-```
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/VoIdemar/golang-interview.git
+
+go 1.19
+
+require (
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/lib/pq v1.10.7 // indirect
+	github.com/xo/dburl v0.13.0 // indirect
+)
